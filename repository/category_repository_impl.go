781e655355bb8d410f229214146471b67106096c
hendychrist <hendy.christian@yahoo.com>
2023-02-22 10:27:31 +0700

    Push After Create Database and Repository not done

---
+++ repository/category_repository_impl.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"hendychrist/belajar-golang-restful-api/helper"
+	"hendychrist/belajar-golang-restful-api/model/domain"
+)
+
+type CategoryRepositoryImpl struct {
+}
+
+func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	SQL := "insert into customer(name) value (?)"
+
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
+}
+
+func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	//TODO implement me
+	panic("implement me")
+}
